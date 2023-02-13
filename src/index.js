841609703cec59f79aea8c66ab4c2ca7c9c21b60
aslamhus <aslam.percy.husain@gmail.com>
2023-02-13 15:09:25 -0800

    add test video to dev-server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import VideoEditor from './VideoEditor/VideoEditor';
 import { FileSelect } from '@aslamhus/fileselect';
 const crop = { width: 5, height: 3 };
+
+let renderDiv;
 window.onload = () => {
+  renderDiv = document.getElementById('render');
   const uploadBtn = document.querySelector('button');
-  uploadBtn.onclick = handleUploadBtnClick;
+
+  const video = document.querySelector('video');
+  const editor = new VideoEditor({ videoSrc: video.src, crop });
+  editor.render(renderDiv);
+  // uploadBtn.onclick = handleUploadBtnClick;
 };
 
 function handleUploadBtnClick(event) {
@@ -11,8 +18,7 @@ function handleUploadBtnClick(event) {
   new FileSelect('video/*').select().then((files) => {
     const [videoFile] = files;
     const blob = URL.createObjectURL(videoFile);
-    const renderDiv = document.getElementById('render');
-    renderDiv.style.width = '450px';
+
     const editor = new VideoEditor({ videoSrc: blob, crop });
     editor.render(renderDiv);
   });
