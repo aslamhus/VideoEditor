d33ee36d96e45b4200d6e575a95983a16c9876fa
aslamhus <aslam.percy.husain@gmail.com>
2023-05-25 16:01:46 -0700

    Feature: types.js for typedefs

---
+++ src/VideoEditor/types.js
@@ -0,0 +1,19 @@
+/**
+ * All types
+ *
+ *
+ *
+ * @typedef {Object} crop
+ * @property {number} w
+ * @property {number} h
+ * @property {number} x
+ * @property {number} y
+ *
+ * @typedef {Object} time
+ * @property {number} in
+ * @property {number} out
+ *
+ * @typedef {Object} transformations
+ * @property {crop} crop - the crop values {w,h,x,y}
+ * @property {time} time - the trim values { in, out }
+ */
